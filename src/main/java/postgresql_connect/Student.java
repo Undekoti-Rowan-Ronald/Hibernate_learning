5ff4ae1928385b1f6b5c0fe58abf50c046ffb2a5
undekoti rowan <undekotirowanronald@gmail.com>
2026-09-12 12:09:32 +0530

    corrected code for updation

--- src/main/java/postgresql_connect/Student.java
+++ src/main/java/postgresql_connect/Student.java
@@ -7,10 +7,11 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 
 @Entity
-
+@Table(name="students") // to change the table name 
 public class Student {
 private String name;
 @Id
+@Column(name="id") //to change the coloum name 
 private int aid;
 private String tech ;
 public String getName() {
